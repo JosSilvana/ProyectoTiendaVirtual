b5cb726fb75d342fc884020f65390341e3ece66f
JosSilvana <joss_silvana1995@hotmail.com>
2020-07-23 20:17:28 -0500

    mostrar imagenes productos

--- app/src/main/java/com/uisrael/ListAdapter.java
+++ app/src/main/java/com/uisrael/ListAdapter.java
@@ -1,13 +1,19 @@
 package com.uisrael;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import androidx.annotation.NonNull;
@@ -18,13 +24,14 @@ public class ListAdapter extends ArrayAdapter<Productos> {
     private List<Productos> listaProductos;
     private Context mcontext;
     private int resourceLayout;
+    private Bitmap loadedImage;
+    private ImageView imageView;
     public ListAdapter(@NonNull Context context, int resource, @NonNull List<Productos> listaProductos) {
         super(context, resource, listaProductos);
         this.listaProductos = listaProductos;
         this.mcontext = context;
         this.resourceLayout = resource;
     }
-
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -33,7 +40,8 @@ public class ListAdapter extends ArrayAdapter<Productos> {
             view = LayoutInflater.from(mcontext).inflate(resourceLayout, null);
 
         Productos productos = listaProductos.get(position);
-        ImageView imagen = view.findViewById(R.id.imgProductos);
+        imageView = view.findViewById(R.id.imgProductos);
+        downloadFile("http://192.168.1.3/RestProyectoTiendaMovil/"+productos.getImagen());
 
         TextView nombre = view.findViewById(R.id.ltvNombre);
         nombre.setText(productos.getNombre());
@@ -48,5 +56,18 @@ public class ListAdapter extends ArrayAdapter<Productos> {
         precio.setText(productos.getPrecio());
 
         return view;
+    }
+    void downloadFile(String imageHttpAddress) {
+        URL imageUrl = null;
+        try {
+            imageUrl = new URL(imageHttpAddress);
+            HttpURLConnection conn = (HttpURLConnection) imageUrl.openConnection();
+            conn.connect();
+            loadedImage = BitmapFactory.decodeStream(conn.getInputStream());
+            imageView.setImageBitmap(loadedImage);
+        } catch (IOException e) {
+            Toast.makeText(mcontext.getApplicationContext(), "Error cargando la imagen: "+e.getMessage(), Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
     }
 }
